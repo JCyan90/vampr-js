4cbff96392d26fde24799493f90f40e144ef1931
Julien Atanassian <julien.atanassian@gmail.com>
2019-09-06 21:23:05 +0000

    first exercise

--- vampire.js
+++ vampire.js
@@ -6,26 +6,48 @@ class Vampire {
     this.creator = null;
   }
 
-  /** Simple tree methods **/
-
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let numberOfVampire = 0;
+    let currentVampire = this;
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampire++;
+    }
+    return numberOfVampire;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
+  }
 
+  /** Tree traversal methods **/
+
+  // Returns the vampire object with that name, or null if no vampire exists with that name
+  vampireWithName(name) {
+    
+  }
+
+  // Returns the total number of vampires that exist
+  get totalDescendents() {
+    
+  }
+
+  // Returns an array of all the vampires that were converted after 1980
+  get allMillennialVampires() {
+    
   }
 
   /** Stretch **/
@@ -35,10 +57,23 @@ class Vampire {
   // For example:
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
-  closestCommonAncestor(vampire) {
+  get ancestors() {
+    let ancestors = [this];
+    let currentVampire = this;
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      ancestors.push(currentVampire);
+    }
+    return ancestors;
+  }
 
+  closestCommonAncestor(vampire) {
+    for (let ancestor of this.ancestors) {
+      if (vampire.ancestors.includes(ancestor)) {
+        return ancestor;
+      }
+    }
   }
 }
 
 module.exports = Vampire;
-
